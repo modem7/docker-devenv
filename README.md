1727d41556597acb181eee6f72f3f52d578d96d9
modem7 <modem7@gmail.com>
2022-10-28 22:09:10 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    Initial commit
    # This is the commit message #2:
    
    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Docker Dev Environments
+
+---
+
+# Requirements
+- Docker
+- Wget or Curl
+
+---
+
+# Usage
+
+## Run the following in your console:
+
+wget:
+```
+wget -qO /tmp/devmenu.sh https://raw.githubusercontent.com/modem7/docker-devenv/master/devmenu.sh && bash /tmp/devmenu.sh
+```
+
+curl:
+```
+curl -s https://raw.githubusercontent.com/modem7/docker-devenv/master/devmenu.sh > /tmp/devmenu.sh && bash /tmp/devmenu.sh
+```
+
+Choose the appropriate option to generate your dev environment. 
+
+---
+
+# Screenshot
+![image](https://user-images.githubusercontent.com/4349962/198712114-093aeb76-883c-4001-b039-1c2433d3acd6.png)
---
+++ devmenu.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+gituser="modem7"
+gitrepo="docker-devenv"
+gitfolder="Environments"
+
+printf "\nChecking if dependencies are installed...\n"
+pkg_list=(docker jq)
+tc() { set ${*,,} ; echo ${*^} ; }
+for pkg in "${pkg_list[@]}"
+do
+  titlecase=$(tc $pkg)
+  isinstalled=$(which $pkg > /dev/null 2>&1)
+    if [ $? -eq 0 ];
+     then
+       printf "$titlecase is installed"
+     else
+       printf "\n$titlecase is not installed...Exiting Script\n"
+       exit
+    fi
+echo ""
+done
+
+PS3='Choose Option: '
+dev_list=$(curl -ks https://api.github.com/repos/$gituser/$gitrepo/contents/$gitfolder | jq --raw-output 'map(.name)| .[]')
+dev_list+=" Prune"
+dev_list+=" Quit"
+echo -e "\nSelect which Dev environment you want:\n"
+echo ""
+select dev_name in ${dev_list}; do
+echo -e "\nYou've selected ${dev_name}"
+echo ""
+lowerdev=$(echo $dev_name | tr '[:upper:]' '[:lower:]')
+    case $dev_name in
+      "$dev_name")
+          echo "Creating $dev_name Environment"
+          DOCKER_BUILDKIT=1 docker build -t $lowerdev:dev https://github.com/$gituser/$gitrepo.git -f /Environments/$dev_name/Dockerfile
+          docker run --rm -it --name "$dev_name"Dev --hostname "$dev_name"Dev "$lowerdev:dev"
+          break
+          ;;
+      "Prune")
+          echo "Clearing Docker cache."
+          docker system prune -af
+          break
+          ;;
+	  "Quit")
+	    echo "Exiting script"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+exit 0
