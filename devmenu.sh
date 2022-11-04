ca337be13403c64f0ba3baceb91cdd9f1eaa1946
Alex <modem7@gmail.com>
2022-11-04 01:14:27 +0000

    updated script to remove eval and added ctrl+c catcher

--- devmenu.sh
+++ devmenu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -euo pipefail
+shopt -s extglob
 
 # Variables
 gituser="modem7"
 gitrepo="docker-devenv"
 gitfolder="Environments"
 buildername="DockerDevBuilder"
+failuremsg=""
 
 # Colours
 RED="\e[31m"
@@ -44,57 +46,62 @@ cat << "EOF"
 =========================================
 EOF
 
+ctrl_c () {
+    echo -e "\nUser pressed Ctrl + C. Exiting script...\n"
+    exit 1
+}
+
+# Trap CTRL+C
+trap ctrl_c INT
+
 PS3="Choose Option: "
 dev_list=($(curl -fks https://api.github.com/repos/$gituser/$gitrepo/contents/$gitfolder | jq '. [] | .name' | tr -d '[]," '))
-dev_list_array="${dev_list[*]}"
-dev_list_array_pipe="${dev_list_array// /|}"
-dev_list+=( "Prune" "Quit" )
+dev_env_options=${dev_list[0]};
+for ((i=1; i<${#dev_list[@]}; i++)); do
+        dev_env_options="$dev_env_options|${dev_list[i]}"
+done
 echo -e "\nSelect an option:\n"
-select dev_name in "${dev_list[@]}"; do
+select dev_name in "${dev_list[@]}" "Prune" "Quit"; do
 echo -e "\nYou've selected ${GREEN}${dev_name}${END}\n"
 lowerdev=$(echo $dev_name | tr '[:upper:]' '[:lower:]')
-    eval "case \"$dev_name\" in
-      "$dev_list_array_pipe")
+    case $dev_name in
+      +($dev_env_options))
           echo "Creating buildx builder..."
           if docker buildx create --use --name "$buildername" > /dev/null 2>&1; then
-              echo ""
-              echo "Builder $buildername created"
+              echo -e "\nBuilder $buildername created..."
             else
-              echo "Builder already created, using $buildername"
-              docker buildx use "DockerDevBuilder"
-              echo ""
+              echo -e "Builder already created, using "$buildername"...\n"
+              docker buildx use "$buildername"
           fi
           echo "Creating $dev_name Environment..."
-          docker buildx build --rm=true --build-arg BUILDKIT_INLINE_CACHE=1 --load -t $lowerdev:dev https://github.com/$gituser/$gitrepo.git#:$gitfolder/$dev_name \
-          && clear \
-          && echo "=========================================" \
-          && echo "Activating $dev_name Dev Environment..." \
-          && echo "Press CTRL + D or type exit to leave the container" \
-          && docker run --rm -it --name "$dev_name"Dev"$RANDOM" --hostname "$dev_name"Dev"$RANDOM" "$lowerdev:dev"
+          docker buildx build --rm=true --build-arg BUILDKIT_INLINE_CACHE=1 --load -t $lowerdev:dev https://github.com/$gituser/$gitrepo.git#:$gitfolder/$dev_name
+          clear
+          echo "========================================="
+          echo "Activating $dev_name Dev Environment..."
+          echo "Press CTRL + D or type exit to leave the container"
+          docker run --rm -it --name "$dev_name"Dev"$RANDOM" --hostname "$dev_name"Dev"$RANDOM" "$lowerdev:dev" & dockerrun_pid=$!
           break
           ;;
       "Prune")
           echo "Clearing Docker cache..."
           docker system prune -af
-          echo ""
-          echo "Removing Docker buildx builder..."
+          echo -e "\nRemoving Docker buildx builder..."
           if docker buildx rm "$buildername" > /dev/null 2>&1; then
-              echo ""
-              echo "Builder $buildername removed"
+              echo -e "\nBuilder $buildername removed"
             else
-              echo "Builder already removed, no action performed"
-              echo ""
-          fi          
+              echo -e "Builder already removed, no action performed\n"
+          fi
           exec bash $0
           ;;
       "Quit")
-          echo "Exiting script"
-          exit
+          echo "Exiting script..."
+          break
           ;;
        *)
-          echo "invalid option $REPLY"
+          echo "Invalid option $REPLY"
+          exec bash $0
           ;;
-    esac"
+    esac
 done
 
 exit 0
